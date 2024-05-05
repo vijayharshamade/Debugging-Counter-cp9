0014bff359a894d6dc06b5e13b1d66a1b623f3f1
Vijay Harsha Made <87754774+vijayharshamade@users.noreply.github.com>
2024-05-05 16:12:51 +0530

    Update index.js

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,28 +1,35 @@
-import Component from 'react'
+// FIX1: The import statement for Component class should be written like this
+import {Component} from 'react'
 
 import './index.css'
 
-class Counter {
+// FIX2: To create a class component we need to extend the React "Component" class
+class Counter extends Component {
   state = {count: 0}
 
   onDecrement = () => {
-    this.state.count = this.state.count - 1;
+    // FIX3: The state should be updated using "setState" method
+    this.setState(prevState => ({count: prevState.count - 1}))
   }
 
   onIncrement = () => {
-    this.state.count = this.state.count + 1;
+    // FIX4: The state should be updated using "setState" method
+    this.setState(prevState => ({count: prevState.count + 1}))
   }
 
   render() {
-    {count} = this.state
+    // FIX5: We should use const to avoid reassigning a value to the variable
+    const {count} = this.state
 
     return (
       <div className="app-container">
         <h1 className="count">Count {count}</h1>
-        <button className="button" onClick={this.onDecrement()} type="button">
+        {/* FIX6: The event handler onIncrement, should be passed as a reference to the onClick attribute */}
+        <button className="button" onClick={this.onIncrement} type="button">
           Increase
         </button>
-        <button className="button" onClick={this.onIncrement()} type="button">
+        {/* FIX7: The event handler onDecrement, should be passed as a reference to the onClick attribute */}
+        <button className="button" onClick={this.onDecrement} type="button">
           Decrease
         </button>
       </div>
@@ -31,3 +38,4 @@ class Counter {
 }
 
 export default Counter
+
